21ba82e04b0079938ff63f6633b2b160b6eb9813
ajlopez <webmaster@ajlopez.com>
2015-02-03 13:55:26 -0300

    Get variables from datasource first tests

--- test/variables.js
+++ test/variables.js
@@ -16,6 +16,13 @@ exports['Create datasource'] = function (test) {
     dsid = result;
 }
 
+exports['Get no variables'] = function (test) {
+    var result = dvalues.getVariables(dsid);
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+}
+
 exports['Add variable'] = function (test) {
     var name = 'var1';
     var unit = 'meters';
@@ -25,3 +32,16 @@ exports['Add variable'] = function (test) {
     var result = dvalues.addVariable(dsid, name, unit, description, properties, tags);
     test.ok(result);
 }
+
+exports['Get variables'] = function (test) {
+    var result = dvalues.getVariables(dsid);
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0].name, 'var1');
+    test.equal(result[0].unit, 'meters');
+    test.equal(result[0].description, 'Variable 1');
+    test.deepEqual(result[0].properties, {});
+    test.deepEqual(result[0].tags, []);
+}
+
